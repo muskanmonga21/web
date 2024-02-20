fbcfe262544bfb96f6c650ff7ce3d6e485bdc12f
muskan monga <muskanmonga521@gmail.com>
2024-02-20 12:30:10 +0530

    web

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("hiiee!!");
+console.log(20*62);
+console.log("Konichiva", "Anneangsehyo");
+console.log(30*35, 20*51);
+//- For Comment
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof 123);
+console.log(typeof "friends");
+console.log(typeof "a")
+let abc =20;
+let asd ="friends";
+console.log(abc,asd);
